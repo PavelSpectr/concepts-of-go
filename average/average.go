1f924b6b6713240e9a5f9ce89c70a27cc88bd2c2
PavelSpectr <titsubishi@yandex.ru>
2025-02-05 14:49:56 +0800

    refactor avg to sements

--- average/average.go
+++ average/average.go
@@ -1,1 +1,23 @@
 package average
+
+import (
+	"concepts-of-go/datafile"
+	"fmt"
+	"log"
+)
+
+func main() {
+	numbers, err := datafile.GetFloats("data.txt") //Загружает файл data.txt, разбирает содержащиеся в нем числа и сохраняет массив.
+	//Если произошла ошибка, программа сообщает о ней и завершает работу.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var sum float64 = 0
+	for _, number := range numbers {
+		sum += number
+	}
+
+	sampleCount := float64(len(numbers))
+	fmt.Printf("Average %0.2f\n", sum/sampleCount)
+}
--- datafile/floats.go
+++ datafile/floats.go
@@ -9,36 +9,34 @@ import (
 // GetFloats читает значение float64 из каждой строки файла.
 // Имя файла с данными передается в аргументе.
 // Функция возвращает массив чисел и любую обнаруженную ошибку.
-func GetFloats(filename string) ([3]float64, error) {
-	var numbers [3]float64 ////Объявление возвращаемого массива.
+func GetFloats(filename string) ([]float64, error) {
+	var numbers []float64 ////Объявление возвращаемого массива.
 
 	file, err := os.Open(filename) //Открывает файл с переданным именем.
 	//Если при открытии файла произошла ошибка, вернуть ее.
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
-
-	i := 0 //Переменная для хранения индекса, по которому должно выполняться присваивание.
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		numbers[i], err = strconv.ParseFloat(scanner.Text(), 64) //Строка, прочитанная из файла, преобразуется в float64.
+		number, err := strconv.ParseFloat(scanner.Text(), 64) //Строка, прочитанная из файла, преобразуется в float64.
 		//Если при преобразовании значения в число произошла ошибка, вернуть ее.
 		if err != nil {
-			return numbers, err
+			return nil, err
 		}
-		i++
+		numbers = append(numbers, number)
 	}
 
 	err = file.Close()
 	//Если при закрытии файла произошла ошибка, вернуть ее.
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
 
 	//Если при сканировании файла произошла ошибка, вернуть ее.
 	if scanner.Err() != nil {
-		return numbers, scanner.Err()
+		return nil, scanner.Err()
 	}
 
 	return numbers, nil //Если выполнение дошло до этой точки, значит, ошибок не было, поэтому программа возвращает массив чисел и значение ошибки «nil».
